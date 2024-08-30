03565f12f5a9d8ff94df95983d995b09e98c4b7f
luanfsouza <luanferreira7777@gmail.com>
2024-08-29 23:00:57 -0300

    components dinamycs propets

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.scss'
+  styleUrl: './big-card.component.scss',
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover: string = "";
+  @Input()
+  cardTitle: string = "";
+  @Input()
+  cardDescription: string = "";
+  
 }
